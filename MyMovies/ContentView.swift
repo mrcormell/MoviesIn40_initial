ed1b9e37883d754efcfd10d89af8bb8e82d9a55a
David Cormell <d.cormell@etoncollege.org.uk>
2024-06-17 12:16:39 +0100

    Adding extra scaffolding for version which can be completed in 40 minutes

--- MyMovies/ContentView.swift
+++ MyMovies/ContentView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var movieResults = MovieResults(results: [])
+    
     var body: some View {
         Text("Hello World")
+    }
+    
+    func getMovies() {
+        URLSession.shared.get(path: "movie/popular", queryItems: [:], defaultValue: MovieResults(results: []), completion: { results in
+            movieResults = results
+        })
     }
 }
 
---
+++ MyMovies/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  MyMovies
+//
+//  Created by Cormell, David - DPC on 17/06/2024.
+//
+
+import Foundation
+
+struct Movie: Decodable {
+    
+}
+
+struct MovieResults: Decodable {
+    let results: [Movie]
+}
